bc31bea8ef5843e6e15d04bb8530c5f457351cce
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 23:08:26 +0330

    Edit Header

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIcon
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {ApiComponent} from "./_pages/api/api.component";
+import {AppComponent} from "./app.component";
 
 export const routes: Routes = [
+  {path:'',component:ApiComponent,pathMatch:'full'},
   {path:'home',component:ApiComponent}
 ];
