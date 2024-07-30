b4462d615ce60cac1bea514c67ee8627d0479e37
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-07-30 23:11:29 +0330

    Create Contact Us Page And Edit Router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {ApiComponent} from "./_pages/api/api.component";
 import {AppComponent} from "./app.component";
+import {ContactUSComponent} from "./_pages/contact-us/contact-us.component";
 
 export const routes: Routes = [
   {path:'',component:ApiComponent,pathMatch:'full'},
-  {path:'home',component:ApiComponent}
+  {path:'home',component:ApiComponent},
+  {path:'contactUS',component:ContactUSComponent}
 ];
